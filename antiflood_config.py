68643c58ed0af6a946afe0775253118f86e4352a
Federico Torrielli <federico.torriell@edu.unito.it>
2020-10-06 16:57:59 +0200

    first

--- antiflood_config.py
+++ antiflood_config.py
@@ -1,4 +1,4 @@
-token = "1328533728:AAE9h8GSbF0OWftLJiimqCW2ZzeTYOK3akM"
+token = "1232177242:AAHGeNAWH6h414TDqIrP1GBWHcjYOp4ncbg"
 settings = {
     'antiflood_seconds': 2,
     'antiflood_max_msgs': 3
